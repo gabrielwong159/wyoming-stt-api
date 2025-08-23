961605e7c6d856898487f73ccd7957bd3ba4f5ad
Gabriel Wong <gabrielwong159@gmail.com>
2025-08-24 00:04:09 +0800

    add wyoming server
    Add a Wyoming server that handles speech-to-text (ASR) events by storing the
    file in an in-memory buffer.
    
    When sending the buffer to the OpenAI SDK for transcription, we need to provide
    a dummy file name, because the API infers the file type from the extension of
    the file name and not the contents of the file.

--- wyoming_stt_api/dependencies.py
+++ wyoming_stt_api/dependencies.py
@@ -1,14 +1,22 @@
+import asyncio
 import logging
 
+from wyoming.server import AsyncTcpServer
+
 from wyoming_stt_api.clients.openai import OpenAIClient
+from wyoming_stt_api.services.wyoming import WyomingEventHandler
 from wyoming_stt_api.settings import Settings
 
 logging.basicConfig(
     level=logging.INFO, format="%(levelname)s %(name)s %(asctime)s %(message)s"
 )
 
-
 settings = Settings()
 openai_client = OpenAIClient(
     api_key=settings.openai_api_key, model=settings.openai_model
 )
+server = AsyncTcpServer(settings.server_host, settings.server_port)
+
+
+def create_handler(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+    return WyomingEventHandler(openai_client, reader, writer)
---
+++ wyoming_stt_api/services/wyoming.py
@@ -0,0 +1,109 @@
+import asyncio
+import logging
+import wave
+from io import BytesIO
+
+from wyoming.asr import Transcribe, Transcript
+from wyoming.audio import AudioChunk, AudioStart, AudioStop
+from wyoming.event import Event
+from wyoming.info import AsrModel, AsrProgram, Attribution, Describe, Info
+from wyoming.server import AsyncEventHandler
+
+from wyoming_stt_api.clients.openai import OpenAIClient
+
+logger = logging.getLogger(__name__)
+
+
+class WyomingEventHandler(AsyncEventHandler):
+    def __init__(
+        self,
+        openai_client: OpenAIClient,
+        reader: asyncio.StreamReader,
+        writer: asyncio.StreamWriter,
+    ):
+        super().__init__(reader, writer)
+        self._openai_client = openai_client
+        self._wave_file: wave.Wave_write | None = None
+        self._buffer: BytesIO = BytesIO()
+
+    async def handle_event(self, event: Event) -> bool:
+        if AudioStart.is_type(event.type):
+            self._start_audio(AudioStart.from_event(event))
+            return True
+
+        if AudioChunk.is_type(event.type):
+            self._add_audio_chunk(AudioChunk.from_event(event))
+            return True
+
+        if AudioStop.is_type(event.type):
+            self._stop_audio()
+            await self._transcribe_buffer()
+            return False
+
+        if Transcribe.is_type(event.type):
+            # TODO: this changes the transcription language, but we ignore that
+            #  and always use English.
+            return True
+
+        if Describe.is_type(event.type):
+            await self._send_info()
+            return True
+
+        return False
+
+    def _start_audio(self, start: AudioStart):
+        self._buffer = BytesIO()
+        self._wave_file = wave.open(self._buffer, "wb")
+        self._wave_file.setframerate(start.rate)
+        self._wave_file.setsampwidth(start.width)
+        self._wave_file.setnchannels(start.channels)
+
+    def _add_audio_chunk(self, chunk: AudioChunk):
+        if self._wave_file is None:
+            raise ValueError("Audio not started")
+
+        self._wave_file.writeframes(chunk.audio)
+
+    def _stop_audio(self):
+        if self._wave_file is None:
+            raise ValueError("Audio not started")
+
+        duration = self._wave_file.getnframes() / self._wave_file.getframerate()
+        logger.info(f"Received audio with duration {duration:.2f}s")
+
+        self._wave_file.close()
+        self._wave_file = None
+
+    async def _transcribe_buffer(self):
+        text = self._openai_client.speech_to_text(self._buffer, file_extension="wav")
+        await self.write_event(Transcript(text=text).event())
+
+    async def _send_info(self):
+        info = Info(
+            asr=[
+                AsrProgram(
+                    name="Speech-to-text API",
+                    description=None,
+                    version=None,
+                    attribution=Attribution(
+                        name="wyoming-stt-api",
+                        url="https://github.com/gabrielwong159/wyoming-stt-api",
+                    ),
+                    installed=True,
+                    models=[
+                        AsrModel(
+                            name=self._openai_client.model_name,
+                            description=None,
+                            version=None,
+                            attribution=Attribution(
+                                name="OpenAI",
+                                url="https://platform.openai.com/docs/models",
+                            ),
+                            installed=True,
+                            languages=["en"],
+                        )
+                    ],
+                )
+            ],
+        )
+        await self.write_event(info.event())
